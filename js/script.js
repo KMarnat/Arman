3bf6caa6604b16240646edaf67a106e7c79566a7
Kaspar Marnat <kasparmarnat@gmail.com>
2023-01-27 14:50:32 +0200

    finished hero section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// window.onscroll = function() {myFunction()};
+
+// var stickyMenu = document.getElementById("sticky-menu");
+// var sticky = stickyMenu.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
